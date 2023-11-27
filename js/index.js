9d0a1ed40f4996ce40da4f0a86f6f0b849375c40
GlukhovKA <glukhov.ka@yandex.ru>
2023-11-27 19:59:36 +0300

    fixes

--- js/index.js
+++ js/index.js
@@ -30,3 +30,116 @@ console.log(owl);
     })
 
 })
+
+$(document).ready(function() {
+	
+	if($.cookie('font_size')){
+		$('*').css('font-size', $.cookie('font_size'));
+	}
+	if($.cookie('show_image') == 'N'){
+		$('img').hide();		
+	}
+	if($.cookie('site_color')){
+		if($.cookie('site_color') == 'wb'){
+			$('*:not(.cecutient *):not(.cecutient)').css('background-color', '#FFF');
+			$('*:not(.cecutient *):not(.cecutient)').css('background-image', 'none');
+			$('*:not(.cecutient *):not(.cecutient)').css('color', '#000');
+		} else if ($.cookie('site_color') == 'bw'){
+			$('*:not(.cecutient *):not(.cecutient)').css('background-color', '#000');
+			$('*:not(.cecutient *):not(.cecutient)').css('background-image', 'none');
+			$('*:not(.cecutient *):not(.cecutient)').css('color', '#FFF');
+		} else if ($.cookie('site_color') == 'bb'){
+			$('*:not(.cecutient *):not(.cecutient)').css('background-color', '#9dd1ff');
+			$('*:not(.cecutient *):not(.cecutient)').css('background-image', 'none');
+			$('*:not(.cecutient *):not(.cecutient)').css('color', '#063462');
+		}	
+	}
+
+
+  $('.box__item-blind').click(function(e) {
+    e.preventDefault();
+    $('.cecutient').fadeToggle();
+  });
+	
+	$('#cecutient_props_usual').click(function(){
+		$('.cecutient').attr('data-fontsize', '15');
+		$('input[name=show_image]').filter('[value="Y"]').attr('checked', true);
+		$('input[name=site_color]').filter('[value="Y"]').attr('checked', false);
+		$('img').show();
+		$('*').css('font-size', null);
+		$('*').css('background-color', null);
+		$('*').css('background-image', null);
+		$('*').css('color', null);
+
+		$.cookie("site_color", false, { expires: 7, path: '/' });
+		$.cookie("show_image", false, { expires: 7, path: '/' });
+		
+		// редирект на текущую страницу. Можно использовать $_SERVER['REQUEST_URI'], если доступно
+		curLocation = '/';
+		window.location.href = curLocation;
+	});
+	
+	$('#special_link').click(function(){
+		if($('#cecutient_props').css('display') == 'none'){
+			$('#cecutient_props').css('display', 'block');
+			$('#cecutient_props_image').css('display', 'block');
+			$('#cecutient_props_usual').css('display', 'block');
+			$('#cecutient_props_color').css('display', 'block');
+		} else {
+			$('#cecutient_props').css('display', 'none');
+			$('#cecutient_props_image').css('display', 'none');
+			$('#cecutient_props_usual').css('display', 'none'); 
+			$('#cecutient_props_color').css('display', 'none');
+		}
+		return false;
+	});
+	
+	$('#fontminus').click(function(){
+		size = $('#fontminus').closest('.cecutient').attr('data-fontsize');
+		size = size - 2;
+		if(size > 7) $('#fontminus').closest('.cecutient').attr('data-fontsize', size);
+		$('*').css('font-size', size);
+		$.cookie("font_size", size, { expires: 7, path: '/' });
+		return false;
+	});
+	
+	$('#fontplus').click(function(){
+		size = $('#fontminus').closest('.cecutient').attr('data-fontsize');
+		size = parseInt(size) + 2;
+		$('#fontminus').closest('.cecutient').attr('data-fontsize', size);
+		$('*').css('font-size', size);
+		$.cookie("font_size", size, { expires: 7, path: '/' });
+		return false;
+	});
+	
+	$('input[name=site_color]').change(function(){
+		if($(this).val() == 'wb'){
+			$('*:not(.cecutient *):not(.cecutient)').css('background-color', '#FFF');
+			$('*:not(.cecutient *):not(.cecutient)').css('background-image', 'none');
+			$('*:not(.cecutient *):not(.cecutient)').css('color', '#000');
+			$.cookie("site_color", 'wb', { expires: 7, path: '/' });
+		} else if ($(this).val() == 'bw'){
+			$('*:not(.cecutient *):not(.cecutient)').css('background-color', '#000');
+			$('*:not(.cecutient *):not(.cecutient)').css('background-image', 'none');
+			$('*:not(.cecutient *):not(.cecutient)').css('color', '#FFF');
+			$.cookie("site_color", 'bw', { expires: 7, path: '/' });
+		} else if ($(this).val() == 'bb'){
+			$('*:not(.cecutient *):not(.cecutient)').css('background-color', '#9dd1ff');
+			$('*:not(.cecutient *):not(.cecutient)').css('background-image', 'none');
+			$('*:not(.cecutient *):not(.cecutient)').css('color', '#063462');
+			$.cookie("site_color", 'bb', { expires: 7, path: '/' });
+		}
+	});
+
+	$('input[name=show_image]').change(function(){
+		if($(this).val() == 'Y'){
+			$('img').show();
+			$.cookie("show_image", 'Y', { expires: 7, path: '/' });
+		} else {
+			$('img').hide();
+			$.cookie("show_image", 'N', { expires: 7, path: '/' });
+		}
+	});
+
+});
+
